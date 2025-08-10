9e8b290720aab02dadc68a06047fcf0a53c814be
jackcoll123 <jackcoll123@gmail.com>
2025-08-10 15:29:06 -0400

    initial commit for calculator app

---
+++ Calculator/index.js
@@ -0,0 +1,43 @@
+
+//document.getElementById("num1-el").textContent = num1
+//document.getElementById("num2-el").textContent = num2
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+let resultEl = document.getElementById("result-el")
+let result = ""
+
+function add() {
+    let num1 = document.getElementById("num1-el").valueAsNumber;
+    let num2 = document.getElementById("num2-el").valueAsNumber;
+    console.log(num1 + " " + num2)
+    result = num1 + num2
+    resultEl.textContent = "Result: " + result
+}
+
+function subtract() {
+    let num1 = document.getElementById("num1-el").valueAsNumber;
+    let num2 = document.getElementById("num2-el").valueAsNumber;
+    result = num1 - num2
+    resultEl.textContent = "Result: " + result
+}
+
+function multiply() {
+    let num1 = document.getElementById("num1-el").valueAsNumber;
+    let num2 = document.getElementById("num2-el").valueAsNumber;
+    result = num1 * num2
+    resultEl.textContent = "Result: " + result
+}
+
+function divide() {
+    let num1 = document.getElementById("num1-el").valueAsNumber;
+    let num2 = document.getElementById("num2-el").valueAsNumber;
+    result = num1 / num2
+    resultEl.textContent = "Result: " + result
+}
