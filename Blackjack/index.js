57f14e3384517bd11202fcf1cec815bee95eda1b
jackcoll123 <jackcoll123@gmail.com>
2025-08-10 19:29:55 -0400

    updates

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,29 +1,34 @@
-
 let firstCard = 10
 let secondCard = 11
+let message = ""
 
 let hasBlackJack = false
 let isAlive = true
-let message = ""
+
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
+
+
 let sum = firstCard + secondCard
 
-
-
 function startgame() {
-
 if (sum <= 20) {
-    message = "Do you want to draw a new card"
+    message = "Do you want to draw a new card?"
+    
 } else if (sum === 21) {
     message = "you win"
     hasBlackJack = true
+
 } else {
     message = "you lose"
     isAlive = false
-}
 
+}
 console.log(message)
+messageEl.textContent = message
 
 }
+
 
 
 
