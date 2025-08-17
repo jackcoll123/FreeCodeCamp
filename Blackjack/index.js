2078fd90415722d5cbed2af63ae86c263b3dc9e1
jackcoll123 <jackcoll123@gmail.com>
2025-08-17 17:34:39 -0400

    Added practice, advanced blackjack game

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,31 +1,56 @@
-let firstCard = 10
-let secondCard = 11
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
 let message = ""
-
 let hasBlackJack = false
 let isAlive = true
-
 let messageEl = document.getElementById("message-el")
-console.log(messageEl)
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.querySelector("#sum-el")
 
+function startGame() {
+    renderGame()
+}
 
-let sum = firstCard + secondCard
-
-function startgame() {
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-    
-} else if (sum === 21) {
-    message = "you win"
-    hasBlackJack = true
-
-} else {
-    message = "you lose"
-    isAlive = false
+function getRandomCard() {
 
 }
-console.log(message)
+
+function renderGame() {
+
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+
+    sumEl.textContent = "Sum: " + sum
+
+    console.log(messageEl)
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+        
+    } else if (sum === 21) {
+        message = "you win"
+        hasBlackJack = true
+
+    } else {
+        message = "you lose"
+        isAlive = false
+
+}
 messageEl.textContent = message
+console.log(message)
+
+}
+
+function newCard() {
+    let drawnCard = getRandomCard()
+    cards.push(drawnCard)
+    sum += drawnCard
+    renderGame()
+
 
 }
 
---
+++ Practice/arrays.js
@@ -0,0 +1,21 @@
+/*
+let cards = [7,4]
+
+cards.push(6)
+console.log(cards)
+
+cards.pop()
+console.log(cards)
+
+
+for (let i = 10; i < 101; i += 10) {
+    console.log(i)
+}
+    */
+
+let cards = [7, 3, 9]
+
+for (i = 0; i < cards.length; i++) {
+    console.log(cards[i])
+
+}
